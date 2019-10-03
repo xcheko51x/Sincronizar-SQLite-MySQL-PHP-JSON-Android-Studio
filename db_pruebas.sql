10cb11a6be0cc5bb939060c5b02ad09ef69304ff
xcheko51x <xcheko51x@gmail.com>
2019-10-02 19:00:28 -0500

    Update db_pruebas.sql

--- db_pruebas.sql
+++ db_pruebas.sql
@@ -2,13 +2,10 @@ create database pruebas;
 
 use pruebas;
 
-create table usuarios(
-	idUsuario int primary key auto_increment,
-	nombre varchar(50) not null,
-	telefono varchar(15) not null,
-	email varchar(30) not null,
-	usuario varchar(50) not null,
-	contrasena varchar(15) not null
+create table ventas(
+	idVenta int primary key auto_increment,
+	producto varchar(50) not null,
+	fechaVenta varchar(50) not null,
+	precio varchar(5) not null,
+	sincronizado varchar(5) not null
 );
-
-insert into usuarios (nombre, telefono, email, usuario, contrasena) values('Ana L', '123456789', 'ana@mail.com', 'ana', 'ana');
